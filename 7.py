ee85a1077bf81b1887134d6c15ea8e91f8a0ae8e
MeisterLLD <dietrich.laurent@gmail.com>
2025-12-07 08:21:58 +0100

    day 7

---
+++ 7.py
@@ -0,0 +1,34 @@
+lignes = open('7').read().splitlines()
+splitters = [ ]
+for i, ligne in enumerate(lignes):
+    for j, char in enumerate(ligne):
+        if char == '^':
+            splitters.append((i,j))
+
+def isactive( pos, splitters):
+    p, q = pos
+    if p == 2:
+        return True
+    y = p-1
+    while (y,q) not in splitters and (y,q-1) not in splitters and (y,q+1) not in splitters:
+        y -= 1
+    return (y,q) not in splitters
+
+
+ans1 = sum([ int(isactive(pos, splitters)) for pos in splitters ])
+print('Part 1: ', ans1)
+
+splitters = set(splitters)
+from functools import cache
+@cache
+
+def nbtimelines( pos):
+    pmax = max( T[0] for T in splitters)
+    p, q = pos
+    if p > pmax:
+        return 1
+
+    if (p, q) not in splitters:
+        return nbtimelines((p+1,q))
+
+    return nbtimelines((p,q-1)) + nbtimelines((p, q+1))
