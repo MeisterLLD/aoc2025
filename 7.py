456b29a5c3934f5d9ff59479086079da5f8d9fae
MeisterLLD <dietrich.laurent@gmail.com>
2025-12-07 13:51:11 +0100

    make part 1 recursive

--- 7.py
+++ 7.py
@@ -11,10 +11,8 @@ def isactive(pos, splitters):
     p, q = pos
     if p == 2:
         return True
-
     if (p-1,q-1) not in splitters and (p-1,q) not in splitters and (p-1,q+1) not in splitters:
         return isactive((p-1,q), splitters)
-
     return (p-1,q) not in splitters
 
 ans1 = sum([ int(isactive(pos, splitters)) for pos in splitters ])
@@ -22,17 +20,14 @@ print('Part 1 :', ans1)
 
 splitters = set(splitters)
 from functools import cache
-
 @cache
 def nbtimelines(pos):
     pmax = max( T[0] for T in splitters)
     p, q = pos
     if p > pmax:
         return 1
-
     if (p, q) not in splitters:
         return nbtimelines((p+1,q))
-
     return nbtimelines((p,q-1)) + nbtimelines((p, q+1))
 
 print('Part 2 :', nbtimelines(start))
