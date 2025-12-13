7fb988f98d6ebee8c34afe30ce3e3bcda49b612d
MeisterLLD <dietrich.laurent@gmail.com>
2025-12-13 07:18:35 +0100

    day 12 joke version

--- 12.py
+++ 12.py
@@ -1,11 +1,9 @@
-sizes = [6, 7, 5, 7, 7, 7]
 data = open('12').read().splitlines()[30:]
 P1 = 0
 for ligne in data:
     str1, str2 = ligne.split(': ')
     L, H = map(int,  str1.split('x')   )
     available = L*H
-    needed = sum(x*sizes[i] for i,x in enumerate(list(map(int, str2.split(' ')))))
-    if needed <= available:
+    if available >= 9*sum((list(map(int, str2.split(' '))))):
         P1 += 1
 print(P1)
